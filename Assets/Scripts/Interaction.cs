9b074c815d66ea248d2ff27d174518c493d287f3
Newton Duarte Cavalcanti <newton_@hotmail.com>
2021-08-12 05:12:48 -0300

    feat: add interaction script

---
+++ Assets/Scripts/Interaction.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Interaction : MonoBehaviour
+{
+    //[SerializeField] internal bool isDoor;
+    // 0 = porta
+    // 1 = loja
+    // 2 = NPC Dialog
+    // 3 = Stash
+    // 4 = ...
+    [SerializeField] internal int interactionId;
+}
